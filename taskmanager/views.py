40d20dce7e43dd899f9a9594272d58a0c6d6df42
JarlTetrazepam <gaterzoom3638@gmail.com>
2021-02-28 15:38:06 +0100

    Adding tasks works fine, may implement deletion

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,13 +1,30 @@
+from django import forms
 from django.shortcuts import render
 from datetime import datetime
 
-tasks = ["test1", "test2", "test3"]
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="Add a new task")
 
 # Create your views here.
 
 def index(request):
-    currentDate = datetime.now().strftime("%A, %d. %b. %Y")
+    current_date = datetime.now().strftime("%A, %d. %b. %Y")
+
+    if "tasks" not in request.session:
+        request.session["tasks"] = []   
+
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            new_task = form.cleaned_data["task"]
+            request.session["tasks"] += [new_task]
+        else:
+            return render(request, "taskmanager/index.html", {
+                "form": form
+            })
+
     return render(request, "taskmanager/index.html", {
-        "tasks": tasks,
-        "currentDate": currentDate
+        "tasks": request.session["tasks"],
+        "currentDate": current_date,
+        "form": NewTaskForm()
     })
